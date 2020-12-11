9bc2fb03bec9b3c5f6edc9aaf935c56660f20d90
Telmo "Trooper <telmo.trooper@gmail.com>
2020-12-11 18:39:38 -0300

    Cleaned up code a bit

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -4,10 +4,9 @@ fn main() {
     let file_content: String = fs::read_to_string("src/input.txt")
         .expect("Unable to read file.");
 
-    let lines: Vec<&str> = file_content.split("\n").collect();
+    let lines: Vec<&str> = file_content.lines().collect();
 
-    let numbers: Vec<i32> = lines.iter().map(|&line| line.parse::<i32>())
-        .filter_map(Result::ok).collect();
+    let numbers: Vec<i32> = lines.iter().filter_map(|&line| line.parse().ok()).collect();
 
     println!("{:?}", numbers);
 }
