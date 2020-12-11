b4e59eda472bf85865fdd10a60e6457300ee436e
Telmo "Trooper <telmo.trooper@gmail.com>
2020-12-11 18:57:47 -0300

    Day 1 done!

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -8,5 +8,16 @@ fn main() {
 
     let numbers: Vec<i32> = lines.iter().filter_map(|&line| line.parse().ok()).collect();
 
-    println!("{:?}", numbers);
+    println!("{:?}\n", numbers);
+
+    let expected_sum = 2020;
+
+    for x in numbers.iter() { // I should probably ignore the pairs that were already checked, but right now this is O(n²).
+        for y in numbers.iter() {
+            if x + y == expected_sum {
+                println!("{:?} + {:?} = {:?}", x, y, expected_sum);
+                println!("{:?} * {:?} = {:?}\n", x, y, x * y);
+            }
+        }
+    }
 }
