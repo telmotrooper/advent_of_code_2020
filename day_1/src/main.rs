743a9344fa6b364c7827e65eb80ef3c46a167de9
Telmo Silva <tvsilva@tech6.com.br>
2020-12-11 17:24:11 -0300

    Made vector of strings

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,7 +1,10 @@
 use std::fs;
 
 fn main() {
-    let contents = fs::read_to_string("src/input.txt");
+    let file_content: String = fs::read_to_string("src/input.txt")
+        .expect("Unable to read file.");
 
-    println!("{:?}", contents);
+    let lines: Vec<&str> = file_content.split("\n").collect();
+
+    println!("{:?}", lines);
 }
