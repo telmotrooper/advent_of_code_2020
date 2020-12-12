181036936938c6fde2dd41ba06f642005df3eb12
Telmo "Trooper <telmo.trooper@gmail.com>
2020-12-12 01:21:45 -0300

    Serializing data into struct

--- day_2_part_1/src/main.rs
+++ day_2_part_1/src/main.rs
@@ -14,21 +14,20 @@ fn main() {
 
     let lines: Vec<&str> = file_content.lines().collect();
 
-    for entry in lines.iter() {
-        let x: Vec<&str> = entry
-        .splitn(2, ": ")
-        .flat_map(|y: &str| y.split("-"))
-        .collect();
+    for line in lines.iter() {
+        let entry_vector: Vec<&str> = line
+            .splitn(2, ": ")
+            .flat_map(|y: &str| y.splitn(2, "-"))
+            .flat_map(|z: &str| z.splitn(2, " "))
+            .collect(); // E.g. ["8", "9", "n", "nnnnnnnnn"]
         
-        println!("{:?}", x);
-        // let mut split = entry.splitn(2, ": "); // E.g. "8-9 n", "nnnnnnnnn"
+        let entry = Entry {
+            minimum: entry_vector[0].parse().unwrap(),
+            maximum: entry_vector[1].parse().unwrap(),
+            character: entry_vector[2].parse().unwrap(),
+            password: entry_vector[3]
+        };
 
-        // let policy: &str = split.next().unwrap();
-
-        // let mut policy_split = policy.splitn(2, "-"); // E.g. "8", "9 n"
-        // let min = policy_split.next().unwrap();
-        // // let min_max: (&str, &str) = 
-        // let password: &str = split.next().unwrap();
-        // println!("{:?} / {:?}", policy, password);
+        println!("{:?}", entry);
     }
 }
