94f199cce45476451dc0fba43e97f0eb71d6fe06
Telmo "Trooper <telmo.trooper@gmail.com>
2020-12-12 01:09:12 -0300

    Trying to deserialize data properly

--- day_2_part_1/src/main.rs
+++ day_2_part_1/src/main.rs
@@ -1,5 +1,13 @@
 use std::fs;
 
+#[derive(Debug)]
+struct Entry<'e> {
+    minimum: u8,
+    maximum: u8,
+    character: char,
+    password: &'e str
+}
+
 fn main() {
     let file_content: String = fs::read_to_string("src/input.txt")
         .expect("Unable to read file.");
@@ -7,8 +15,20 @@ fn main() {
     let lines: Vec<&str> = file_content.lines().collect();
 
     for entry in lines.iter() {
-        let mut split = entry.splitn(2, ": ");
-        let tuple: (&str, &str) = (split.next().unwrap(), split.next().unwrap());
-        println!("{:?}", tuple);
+        let x: Vec<&str> = entry
+        .splitn(2, ": ")
+        .flat_map(|y: &str| y.split("-"))
+        .collect();
+        
+        println!("{:?}", x);
+        // let mut split = entry.splitn(2, ": "); // E.g. "8-9 n", "nnnnnnnnn"
+
+        // let policy: &str = split.next().unwrap();
+
+        // let mut policy_split = policy.splitn(2, "-"); // E.g. "8", "9 n"
+        // let min = policy_split.next().unwrap();
+        // // let min_max: (&str, &str) = 
+        // let password: &str = split.next().unwrap();
+        // println!("{:?} / {:?}", policy, password);
     }
 }
