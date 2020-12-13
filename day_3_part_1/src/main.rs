5fd5526a0fe6caa66f9d78291d3b4cf3e100da11
Telmo "Trooper <telmo.trooper@gmail.com>
2020-12-13 00:15:42 -0300

    Day 3 part 1 done!

--- day_3_part_1/src/main.rs
+++ day_3_part_1/src/main.rs
@@ -1,5 +1,11 @@
 use std::fs;
 
+#[derive(Debug)]
+struct Position {
+    x: usize,
+    y: usize
+}
+
 fn main() {
     let file_content: String = fs::read_to_string("src/input.txt")
         .expect("Unable to read file.");
@@ -19,14 +25,23 @@ fn main() {
     println!("The map is {:?}x{:?} in size.", x_limit, y_limit);
 
     let mut trees_found = 0;
-    let mut current_pos: (usize, usize) = (0,0);
+    let mut current_pos = Position { x: 0, y: 0 };
 
-    let mut i = 0;
+    while current_pos.y < y_limit - 1 {
+        current_pos = Position {
+            x: (current_pos.x + 3) % x_limit,
+            y: current_pos.y + 1
+        };
 
-    while i < map.len() {
-        // println!("Going further.");
-        i += 1;
+        let location = map
+            .iter().nth(current_pos.y).unwrap()
+            .iter().nth(current_pos.x).unwrap();
+
+        if *location == '#' {
+            trees_found += 1;
+            println!("Tree found at position ({:?},{:?}).", current_pos.x, current_pos.y);
+        }
     }
 
-    println!("{:?}\n", map);
+    println!("{:?} tree(s) found.\n", trees_found);
 }
