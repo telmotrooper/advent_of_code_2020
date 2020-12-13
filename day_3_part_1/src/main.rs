4244045c655c9c937cf6da14f3cc9ff2bd8430ef
Telmo "Trooper <telmo.trooper@gmail.com>
2020-12-12 22:48:33 -0300

    Added base for Day 3 Part 1

---
+++ day_3_part_1/src/main.rs
@@ -0,0 +1,23 @@
+use std::fs;
+
+fn main() {
+    let file_content: String = fs::read_to_string("src/input.txt")
+        .expect("Unable to read file.");
+
+    let lines: Vec<&str> = file_content.lines().collect();
+
+    println!("{:?}\n", lines);
+
+    // let numbers: Vec<i32> = lines.iter().filter_map(|&line| line.parse().ok()).collect();
+
+    // let expected_sum = 2020;
+
+    // for x in numbers.iter() { // I should probably ignore the pairs that were already checked, but right now this is O(n²).
+    //     for y in numbers.iter() {
+    //         if x + y == expected_sum {
+    //             println!("{:?} + {:?} = {:?}", x, y, expected_sum);
+    //             println!("{:?} * {:?} = {:?}\n", x, y, x * y);
+    //         }
+    //     }
+    // }
+}
