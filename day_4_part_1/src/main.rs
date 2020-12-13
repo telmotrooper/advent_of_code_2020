fde8a09a2fbbf4d13e12d46e5912dd2fa97557ba
Telmo "Trooper <telmo.trooper@gmail.com>
2020-12-13 20:00:35 -0300

    Structuring data

--- day_4_part_1/src/main.rs
+++ day_4_part_1/src/main.rs
@@ -1,3 +1,16 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let file_content: String = fs::read_to_string("src/input.txt")
+        .expect("Unable to read file.");
+
+    let lines: Vec<&str> = file_content.lines().filter(|x| *x != "").collect();
+
+    for line in lines {
+        let vec: Vec<&str> = line
+            .split(" ")
+            .collect(); // E.g. ["ecl:gry", "pid:860033327", "eyr:2020", "hcl:#fffffd"]
+
+        println!("{:?}\n", vec);
+    }
 }
