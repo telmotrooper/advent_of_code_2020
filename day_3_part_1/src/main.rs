19230da865920f2501690f593618cc902ded3134
Telmo "Trooper <telmo.trooper@gmail.com>
2020-12-12 23:59:27 -0300

    Getting the size of the grid and instantiating required variables.

--- day_3_part_1/src/main.rs
+++ day_3_part_1/src/main.rs
@@ -13,5 +13,20 @@ fn main() {
         map.push(line.chars().collect());       
     }
 
+    let x_limit = map.iter().nth(0).unwrap().len();
+    let y_limit = map.len();
+
+    println!("The map is {:?}x{:?} in size.", x_limit, y_limit);
+
+    let mut trees_found = 0;
+    let mut current_pos: (usize, usize) = (0,0);
+
+    let mut i = 0;
+
+    while i < map.len() {
+        // println!("Going further.");
+        i += 1;
+    }
+
     println!("{:?}\n", map);
 }
